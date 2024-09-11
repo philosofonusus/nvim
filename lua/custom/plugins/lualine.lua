4f3fc62637082e5fbdf996baf2622b58bfc50ec6
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-09-11 13:16:36 +0200

    feat(lualine): add extensions + fix indicators

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -7,14 +7,21 @@ return {
         options = {
           theme = 'catppuccin',
         },
+        extensions = {
+          'lazy',
+          'quickfix',
+          'oil',
+          'fugitive',
+          'trouble',
+        },
         sections = {
           lualine_c = {
             'filename',
             {
               'harpoon2',
               icon = '󰛢',
-              indicators = { ' 1', '2', '3', '4' },
-              active_indicators = { ' [1]', '[2]', '[3]', '[4]' },
+              indicators = { '1', '2', '3', '4' },
+              active_indicators = { '[1]', '[2]', '[3]', '[4]' },
               color_active = { fg = '#D0A8E5' },
               _separator = ' ',
               no_harpoon = 'Harpoon not loaded',
