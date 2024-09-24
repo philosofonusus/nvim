85a17ae578eb6023e41e8ab5d3a2005eab936a6c
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-09-24 09:14:39 +0200

    fix(oil): check if oil can be launched on startup

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,3 +1,48 @@
+-- taken from https://github.com/goolord/alpha-nvim/blob/b6f4129302db197a7249e67a90de3f2b676de13e/lua/alpha.lua#L570
+-- stylua: ignore start
+local function should_skip_oil()
+    -- don't start when opening a file
+    if vim.fn.argc() > 0 then return true end
+
+    -- Do not open oil if the current buffer has any lines (something opened explicitly).
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 2, false)
+    if #lines > 1 or (#lines == 1 and lines[1]:len() > 0) then return true end
+
+    -- Skip when there are several listed buffers.
+    for _, buf_id in pairs(vim.api.nvim_list_bufs()) do
+        local bufinfo = vim.fn.getbufinfo(buf_id)
+        if bufinfo.listed == 1 and #bufinfo.windows > 0
+            then return true
+        end
+    end
+
+    -- Handle nvim -M
+    if not vim.o.modifiable then return true end
+
+    ---@diagnostic disable-next-line: undefined-field
+    for _, arg in pairs(vim.v.argv) do
+        -- whitelisted arguments
+        -- always open
+        if arg == "--startuptime"
+        then return false
+        end
+
+        -- blacklisted arguments
+        -- always skip
+        if arg == "-b"
+            -- commands, typically used for scripting
+            or arg == "-c" or vim.startswith(arg, "+")
+            or arg == "-S"
+        then return true
+        end
+    end
+
+    -- base case: don't skip
+    return false
+end
+
+-- stylua: ignore end
+
 return {
   {
     'stevearc/oil.nvim',
@@ -25,7 +70,7 @@ return {
         },
       }
 
-      if vim.fn.argv(0) == '' then
+      if should_skip_oil() == false then
         vim.cmd 'Oil'
       end
     end,
