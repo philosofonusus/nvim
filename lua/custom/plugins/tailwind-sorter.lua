d969685463446b0cd819b0a17e3cc1c375b9a430
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-10-02 09:07:49 +0200

    fix(tailwind): trim spaces do not work correctly

--- lua/custom/plugins/tailwind-sorter.lua
+++ lua/custom/plugins/tailwind-sorter.lua
@@ -7,7 +7,6 @@ return {
     config = function()
       require('tailwind-sorter').setup {
         on_save_enabled = true, -- If `true`, automatically enables on save sorting.
-        trim_spaces = true,
         on_save_pattern = { '*.html', '*.js', '*.jsx', '*.tsx', '*.twig', '*.hbs', '*.php', '*.heex', '*.astro' }, -- The file patterns to watch and sort.
         node_path = 'node',
       }
