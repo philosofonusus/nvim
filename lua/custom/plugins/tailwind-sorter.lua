2e4d5f3ae672675626c22cfbfbc24924b3a012fb
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-10-02 07:26:15 +0200

    fix(tailwind): error on initial file open

--- lua/custom/plugins/tailwind-sorter.lua
+++ lua/custom/plugins/tailwind-sorter.lua
@@ -3,10 +3,11 @@ return {
     'laytan/tailwind-sorter.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
     build = 'cd formatter && npm ci && npm run build',
-    event = 'VeryLazy',
+    event = 'BufEnter',
     config = function()
       require('tailwind-sorter').setup {
         on_save_enabled = true, -- If `true`, automatically enables on save sorting.
+        trim_spaces = true,
         on_save_pattern = { '*.html', '*.js', '*.jsx', '*.tsx', '*.twig', '*.hbs', '*.php', '*.heex', '*.astro' }, -- The file patterns to watch and sort.
         node_path = 'node',
       }
