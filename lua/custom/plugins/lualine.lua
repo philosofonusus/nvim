a90adaba2d412f2ceaa8d2eebf898a197d1da2c5
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-10-15 11:53:43 +0200

    feat(lualine): replace default oil extension with custom one
    replaces default oil extension from lualine to include harpoon lualine
    section for easier navigation between harpoon marks when opening oil

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,3 +1,33 @@
+local harpoon_section = {
+  'harpoon2',
+  icon = '󰛢',
+  indicators = { '1', '2', '3', '4' },
+  active_indicators = { '[1]', '[2]', '[3]', '[4]' },
+  color_active = { fg = '#D0A8E5' },
+  _separator = ' ',
+  no_harpoon = 'Harpoon not loaded',
+}
+
+-- instead of using default oil extension we use custom to include harpoon line for easier navigation when opening oil
+local oil_extension = {
+  sections = {
+    lualine_a = {
+      function()
+        local ok, oil = pcall(require, 'oil')
+        if ok then
+          return vim.fn.fnamemodify(oil.get_current_dir(), ':~')
+        else
+          return ''
+        end
+      end,
+    },
+    lualine_c = {
+      harpoon_section,
+    },
+  },
+  filetypes = { 'oil' },
+}
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -10,21 +40,13 @@ return {
         extensions = {
           'lazy',
           'quickfix',
-          'oil',
+          oil_extension,
           'trouble',
         },
         sections = {
           lualine_c = {
             'filename',
-            {
-              'harpoon2',
-              icon = '󰛢',
-              indicators = { '1', '2', '3', '4' },
-              active_indicators = { '[1]', '[2]', '[3]', '[4]' },
-              color_active = { fg = '#D0A8E5' },
-              _separator = ' ',
-              no_harpoon = 'Harpoon not loaded',
-            },
+            harpoon_section,
           },
         },
       }
