d9821b9a50a738b5ec3f10a155aadff72085994a
tentacles <my.tentacles.are.in.you@gmail.com>
2024-12-11 14:13:11 +0100

    feat(debugprint): change to timber

--- lua/custom/plugins/debugprint.lua
+++ lua/custom/plugins/debugprint.lua
@@ -1,8 +1,15 @@
 return {
   {
-    'andrewferrier/debugprint.nvim',
-    dependencies = {
-      'echasnovski/mini.nvim',
-    },
+    'Goose97/timber.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('timber').setup {
+        log_watcher = {
+          enabled = true,
+          sources = {},
+          preview_snippet_length = 32,
+        },
+      }
+    end,
   },
 }
