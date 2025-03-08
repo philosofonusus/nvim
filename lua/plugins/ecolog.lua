e6e6bd482a845b7332ec6239792186de74bb42bb
tentacles <my.tentacles.are.in.you@gmail.com>
2025-03-08 17:03:21 +0100

    ecolog update

--- lua/plugins/ecolog.lua
+++ lua/plugins/ecolog.lua
@@ -2,6 +2,8 @@ return {
   {
     dir = '~/projects/ecolog.nvim',
     keys = {
+      { '<leader>el', '<Cmd>EcologShelterLinePeek<cr>', desc = 'Ecolog peek line' },
+      { '<leader>eh', '<Cmd>EcologShellToggle<cr>', desc = 'Toggle shell variables' },
       { '<leader>ge', '<cmd>EcologGoto<cr>', desc = 'Go to env file' },
       { '<leader>ec', '<cmd>EcologSnacks<cr>', desc = 'Open a picker' },
       { '<leader>eS', '<cmd>EcologSelect<cr>', desc = 'Switch env file' },
@@ -11,6 +13,35 @@ return {
     opts = {
       preferred_environment = 'local',
       types = true,
+      providers = {
+        {
+          pattern = '{{[%w_]+}}?$',
+          filetype = 'http',
+          extract_var = function(line, col)
+            local utils = require 'ecolog.utils'
+            return utils.extract_env_var(line, col, '{{([%w_]+)}}?$')
+          end,
+          get_completion_trigger = function()
+            return '{{'
+          end,
+        },
+      },
+      interpolation = {
+        enabled = true,
+        features = {
+          commands = false,
+        },
+      },
+      sort_var_fn = function(a, b)
+        if a.source == 'shell' and b.source ~= 'shell' then
+          return false
+        end
+        if a.source ~= 'shell' and b.source == 'shell' then
+          return true
+        end
+
+        return a.name < b.name
+      end,
       integrations = {
         lspsaga = true,
         nvim_cmp = true,
@@ -21,6 +52,10 @@ return {
       },
       shelter = {
         configuration = {
+          mask_length = 5,
+          patterns = {
+            ['DATABASE_URL'] = 'full',
+          },
           sources = {
             ['.env.example'] = 'none',
           },
