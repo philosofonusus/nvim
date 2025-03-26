d3daf0bb12e396eae5fd0bc7e0cfd241fa0fde19
TENTACLE <32138582+philosofonusus@users.noreply.github.com>
2025-03-26 12:46:36 +0100

    update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -16,7 +16,6 @@ return {
     version = 'v0.*', -- use a release tag to download pre-built binaries
     opts = {
       keymap = {
-        preset = 'enter',
         ['<C-n>'] = { 'select_next', 'fallback' },
         ['<C-p>'] = { 'select_prev', 'fallback' },
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
@@ -34,10 +33,6 @@ return {
         providers = {
           ecolog = { score_offset = 101, name = 'ecolog', module = 'ecolog.integrations.cmp.blink_cmp' },
           supermaven = { score_offset = 100, async = true, name = 'supermaven', module = 'blink.compat.source' },
-          -- lsp = { score_offset = 4, name = 'lsp', module = 'blink.cmp.sources.lsp' },
-          -- snippets = { score_offset = 3, name = 'snippets', module = 'blink.cmp.sources.snippets' },
-          -- path = { score_offset = 2, name = 'path', module = 'blink.cmp.sources.path' },
-          -- buffer = { score_offset = 1, name = 'buffer', module = 'blink.cmp.sources.buffer' },
         },
         default = { 'ecolog', 'supermaven', 'lsp', 'snippets', 'path', 'buffer' },
         per_filetype = {
